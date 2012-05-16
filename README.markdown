95162131063c9b0e88e4c2a892f2506724441314
Rehno Lindeque <errantkid@gmail.com>
2012-05-16 00:28:00 -0700

    Set up the initial project

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# supersimple-editor.js
+
+**supersimple.js** is a super simple, but highly extendable text/code editor.
+
+## Usage
+
+Currently unreleased homefry!
---
+++ src/header.js
@@ -0,0 +1,8 @@
+/*
+ * supersimple-editor.js - A super... simple... editor (in js)
+ * supersimple-editor.js is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
+ * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
+ */
+var supersimple = supersimple || {};
+(function() {
+"use strict";
